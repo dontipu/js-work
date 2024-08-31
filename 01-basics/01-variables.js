9fc962891f2758e26fa18a65b811f44f4b2c4c8a
Ashish Dangi <141322556+dontipu@users.noreply.github.com>
2024-08-31 09:23:59 +0000

    Mast javascript hai

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,15 @@
+const accountId = 15455
+let accountEmail ="AshishDangi@google.com"
+var accountpssword = "12212121"
+ accountcity ="Gaya"
+//accountId = 2 // not allowed
+ accountEmail = "ac@kc.com"
+accountpssword = "121212121"
+accountcity = "Patna"
+let accountState;
+
+console.log(accountId);
+ 
+console.table([accountId, accountEmail, accountpssword, accountcity, accountState])
+
+
